ae12fc232fc02b5f6d0e49c7a74245724c32c2c5
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2026-03-16 03:57:05 +0000

    simplify code

--- src/main/java/unittestjava/Compute.java
+++ src/main/java/unittestjava/Compute.java
@@ -6,59 +6,10 @@ public final class Compute {
     }
 
     public static int myadd(int a, int b) {
-        if (a > 0 && b > 0) {
-            return a + b;
-        }
-
-        if (a < 0 && b < 0) {
-            return a + b;
-        }
-
-        if (a == 0) {
-            return b;
-        }
-
-        if (b == 0) {
-            return a;
-        }
-
-        if (a > 0 && b < 0) {
-            if (Math.abs(a) > Math.abs(b)) {
-                return a + b;
-            }
-            if (Math.abs(a) < Math.abs(b)) {
-                return a + b;
-            }
-        }
-
-        return 0;
+        return a + b;
     }
 
     public static int mymult(int a, int b) {
-        if (a > 0 && b > 0) {
-            return a * b;
-        }
-
-        if (a < 0 && b < 0) {
-            return a * b;
-        }
-
-        if (a > 0 && b < 0) {
-            return a * b;
-        }
-
-        if (a < 0 && b > 0) {
-            return a * b;
-        }
-
-        if (a == 0) {
-            return 0;
-        }
-
-        if (b == 0) {
-            return 0;
-        }
-
-        return -100;
+        return a * b;
     }
 }
